69a51e73db7b0a9af1f4123fb4ad30fb3e465f89
Edinei <galera.org@gmail.com>
2018-03-30 12:40:46 -0300

    menos requests

--- assets/form-child.js
+++ assets/form-child.js
@@ -2,12 +2,8 @@ $(function() {
     if($("head").find("script[data-info='form-crud']").length === 0) {
         var ran = Math.floor((Math.random() * 10000));
         $("head")
-            .append('<link rel="stylesheet" href="' + HOME + 'vendor/conn/form-crud/assets/dropzone.min.css?v=' + ran + '" >')
-            .append('<link rel="stylesheet" href="' + HOME + 'vendor/conn/form-crud/assets/form.min.css?v=' + ran + '" >')
-            .append('<link rel="stylesheet" href="' + HOME + 'vendor/conn/form-crud/assets/jquery-te.min.css?v=' + ran + '" >')
-            .append('<script src="' + HOME + 'vendor/conn/form-crud/assets/jquery-te.min.js?v=' + ran + '" ></script>')
-            .append('<script src="' + HOME + 'vendor/conn/form-crud/assets/dropzone.min.js?v=' + ran + '" ></script>')
-            .append('<script src="' + HOME + 'vendor/conn/form-crud/assets/form.min.js?v=' + ran + '" data-info="form-crud" ></script>');
+            .append('<link rel="stylesheet" href="' + HOME + 'vendor/conn/form-crud/assets/main.min.css?v=' + ran + '" >')
+            .append('<script src="' + HOME + 'vendor/conn/form-crud/assets/main.min.js?v=' + ran + '" data-info="form-crud" ></script>');
     }
 
     $(".table-search").prop("disabled", true);
