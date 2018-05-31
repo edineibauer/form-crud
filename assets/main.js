ceb873e1ca42a5d8ef4a75a911f39e1f04cd842f
Edinei <galera.org@gmail.com>
2018-05-31 01:01:17 -0300

    up control error

--- assets/main.js
+++ assets/main.js
@@ -2221,7 +2221,7 @@ if (typeof loadMask !== 'function') {
 }
 if (typeof openPanel !== 'function') {
     function openPanel(entity, $id, value, $this) {
-        $this.panel(themeDashboard("<i class='material-icons left padding-right'>create</i><span class='left'>editando</span>", {
+        $this.panel(themeDashboard("<span class='left color-text-grey'>editando</span>", {
             lib: 'form-crud',
             file: 'api',
             entity: entity,
